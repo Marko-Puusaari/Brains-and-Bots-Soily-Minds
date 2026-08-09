292d80b4822cd2c38376564bdafbe41ff3c7f774
Georgia D <250648067+open-to-work-pls-hire-me@users.noreply.github.com>
2026-08-09 14:05:27 +1000

    Add files via upload

--- preset.py
+++ preset.py
@@ -1,14 +1,11 @@
+
 import serial
 import time
-arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+arduino = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 def output():
 	print(arduino.readline().decode('utf-8').strip())
 def pause():
-	time.sleep(4) #for easy testing, being able to change all pauses at once
-
-arduino.write(b'ON\n')
-pause()
-output()
+	time.sleep(5) #for easy testing, being able to change all pauses at once
 
 arduino.write(b'ARM_UP\n')
 pause()
@@ -18,12 +15,31 @@ arduino.write(b'ARM_DOWN\n')
 pause()
 output()
 
+arduino.write(b'STEP_FORWARDS\n')
+pause()
+output()
+
+arduino.write(b'QUARTER_TURN_LEFT')
+pause()
+output()
+
+arduino.write(b'QUARTER_TURN_RIGHT')
+pause()
+output()
+
+arduino.write(b'ARM_DOWN')
+pause()
+output()
+
 arduino.write(b'SENSE_WATER\n')
+pause()
+output()
+
+arduino.write(b'ARM_UP\n')
 pause()
 output()
 
 arduino.write(b'OFF\n')
 pause()
 output()
-pause()
 output()
